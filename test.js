f974f55cc5d5ff6223dc8849661e91635edff811
saymycheese <chi.pham@newschool.edu>
2023-04-13 17:55:56 -0400

    update background

--- test.js
+++ test.js
@@ -7,23 +7,7 @@ const imageArray1 = [
   "assets/images/outfits/top/top6.png"
 ];
 
-
-const image1 = document.querySelector('.img-top');
-const button1 = document.querySelector('.button-top');
-
-
-window.onload = () => generateRandomPicture(imageArray1);
-
-button1.addEventListener("click", () => generateRandomPicture(imageArray1));
-
-function generateRandomPicture(array){
-	let randomNum = Math.floor(Math.random() * array.length); 
-	image1.setAttribute('src', array[randomNum]);
-}
-
-
-
-const imageArray2 = [
+const imageArrayBottom = [
 	"assets/images/outfits/bottom/bottom1.png",
   "assets/images/outfits/bottom/bottom2.png",
   "assets/images/outfits/bottom/bottom3.png",
@@ -32,16 +16,62 @@ const imageArray2 = [
   "assets/images/outfits/bottom/bottom6.png"
 ];
 
-const image2 = document.querySelector(".img-bottom");
-const button2 = document.querySelector(".button-bottom");
+const topList = document.getElementById("topList");
+const bottomList = document.getElementById("bottomList")
+// addAll(topList, imageArray1) 
+// addAll(bottomList, imageArrayBottom)
 
 
-window.onload = () => generateRandomPicture(imageArray2);
-
-button2.addEventListener("click", () => generateRandomPicture(imageArray2));
-
-
-function generateRandomPicture(array){
-	let randomNum = Math.floor(Math.random() * array.length); 
-	image2.setAttribute('src', array[randomNum]);
+// Generate random picture takes a list_select pointer to the html elelemnt and list of picture
+// It will output a random picture from the list of picture and add it to the list emlement list_selector point to 
+function addAll(list_select, listPicture) {
+  list_select.innerHTML = '';
+  for (const element of listPicture) {
+    let newListItem = document.createElement('li');
+    let newImage = document.createElement('img');
+    newImage.src = element; 
+    newListItem.appendChild(newImage);
+    list_select.appendChild(newListItem);
+  }
 }
+
+function generateARandomPicture(list_select, listPicture) {  
+  let newImage = document.createElement('img');
+  let newListItem = document.createElement('li');
+  let currentIndex = 0;
+  newImage.src = listPicture[currentIndex]; 
+  newListItem.appendChild(newImage);
+  list_select.appendChild(newListItem);
+  listPicture.sort(() => Math.random() - 0.5);
+  // Function to change the image at a 0.2-second interval
+  let intervalId = setInterval(() => {
+    // Get the next image URL
+    currentIndex = (currentIndex + 1) % listPicture.length;
+    let nextImage = listPicture[currentIndex];
+    // Set the image source to the next image URL
+    newImage.src = nextImage;
+  }, 400);
+  
+  // Stop changing the image after 3 seconds
+  setTimeout(() => {
+    clearInterval(intervalId);
+  }, 1200);
+}
+
+
+
+
+function generateTop() {
+  topList.innerHTML = '';
+  generateARandomPicture(topList, imageArray1)
+}
+
+function generateBottom() {
+  bottomList.innerHTML = '';
+  generateARandomPicture(bottomList, imageArrayBottom)
+} 
+
+function generateAll() {
+  generateBottom();
+  generateTop();
+} 
